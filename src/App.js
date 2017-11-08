e677cc7dc276a07fd293da9f5dfb1e96d0f38222
trig010 <trig.code@gmail.com>
2017-11-07 23:42:22 -0600

    working on header and nav

--- src/App.js
+++ src/App.js
@@ -7,15 +7,45 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="top-header">
-          <nav className="top-nav">
-            <a href="" className="nav-login">LOGIN</a>
-            <p className="nav-phone">CALL US  1-888-ZINGO-55</p>
-            <a href=""><img src={facebook} className="nav-facebook" alt="facebook"/></a>
+        <header>
+          <div className="top-header">
+            <ul id="top-header-list">
+              <li>
+                <a href="" className="login">LOGIN</a>
+              </li>
+              <li>
+                <p className="phone">CALL US  1-888-ZINGO-55</p>
+              </li>
+              <li>
+                <a href=""><img src={facebook} className="facebook" alt="facebook"/></a>
+              </li>
+            </ul>
+          </div>
+          <div className="logo-top" alt="logo" />
+          <nav className="main-nav">
+            <ul id="nav-list">
+              <li>
+                <a href="" className="second">INSTRUCTIONS</a>
+              </li>
+              <li>
+                <a href="" className="second">RATES & TERMS</a>
+              </li>
+              <li>
+                <a href="" className="second">LEARN MORE</a>
+              </li>
+              <li>
+                <a href="" className="second">CONTACT US</a>
+              </li>
+              <li>
+                <a href="" className="second">FAQ</a>
+              </li>
+              <li>
+                <button className="second">APPLY TODAY</button>
+              </li>
+            </ul>
           </nav>
-          <img src={sprite} className="App-logo" alt="logo" />
         </header>
-        <div className="App-intro">
+        <div>
           <h1 className="title">We offer Loans for Bad Credit</h1>
           <p className="sub-title">We specialize in loans for people with bad credit. Get a loan with Zingo Cash's quick loan process.</p>
           <button className="enroll">ENROLL TODAY</button>
